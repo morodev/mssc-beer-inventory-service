a23fa21e86bfec257865f69528bd1299e0894d0d
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-01-24 18:51:57 +0100

    Initial commit

---
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -0,0 +1,15 @@
+package guru.sfg.beer.inventory.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by Luca Moro on 24/01/2021.
+ */
+@SpringBootApplication
+public class BeerInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerInventoryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package guru.sfg.beer.inventory.service.web.mappers;
+
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by Luca Moro on 24/01/2021.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
