a7d7bf93394e1ca39ccd5d3670d9a05c8da17bf0
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-02-06 18:53:37 +0100

    165. 166. Assignment - Database Initialization, Correction - Hibernate Dialog

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -2,10 +2,12 @@ package guru.sfg.beer.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+
 /**
  * Created by Luca Moro on 24/01/2021.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerInventoryApplication {
 
     public static void main(String[] args) {
